cfa5074b22429340e8586cb49b6a3c25df61e6f3
suri.kim <suri.kim@daumkakao.com>
2018-04-09 19:01:18 +0900

    python matplotlib.pyplot tutorial 따라가기

--- pyplot_05_logarithmic_and_other_nonlinear_axes.py
+++ pyplot_05_logarithmic_and_other_nonlinear_axes.py
@@ -1,10 +1,8 @@
 ########################################################
 # Logarithmic and other nonlinear axes
 ########################################################
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 from matplotlib.ticker import NullFormatter  # useful for `logit` scale
 
 # Fixing random state for reproducibility
@@ -26,14 +24,12 @@ plt.yscale('linear')
 plt.title('linear')
 plt.grid(True)
 
-
 # log
 plt.subplot(222)
 plt.plot(x, y)
 plt.yscale('log')
 plt.title('log')
 plt.grid(True)
-
 
 # symmetric log
 plt.subplot(223)
@@ -48,12 +44,13 @@ plt.plot(x, y)
 plt.yscale('logit')
 plt.title('logit')
 plt.grid(True)
+
 # Format the minor tick labels of the y-axis into empty strings with
 # `NullFormatter`, to avoid cumbering the axis with too many labels.
 plt.gca().yaxis.set_minor_formatter(NullFormatter())
 # Adjust the subplot layout, because the logit one may take more space
 # than usual, due to y-tick labels like "1 - 10^{-3}"
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+plt.subplots_adjust(top=0.99, bottom=0.01, left=0.10, right=0.98, hspace=0.25,
                     wspace=0.35)
 
 plt.show()
